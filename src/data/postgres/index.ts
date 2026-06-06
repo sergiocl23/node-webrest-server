700d4fc2327df2304dbc7028814d870647e09ff7
Sergio Cerda <sergio.cerda.lozano@gmail.com>
2026-06-06 18:36:48 -0400

    Fin seccion 17

--- src/data/postgres/index.ts
+++ src/data/postgres/index.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../../generated/prisma/client";
+import { PrismaClient } from "../../generated/prisma/client";
 
 const adapter = new PrismaPg(process.env.POSTGRES_URL!);
 export const prisma = new PrismaClient({ adapter });
